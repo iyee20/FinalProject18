31fba09e55c222977a133d115e90ec4fab513d58
Wai Yee <iyee@ucvts.org>
2018-12-23 20:13:51 -0500

    Start pygame. Create first Surface.

--- projectcode.py
+++ projectcode.py
@@ -1,6 +1,12 @@
 #this is the text-based form of "Fire Emblem: Let's Get This Bread"
 #the game is heavily based on Fire Emblem Heroes, the mobile game of the Fire Emblem series
 
+import sys, pygame #import pygame to use
+pygame.init()
+
+size = width, height = 100, 100 #define size as a 100x100 px area
+screen = pygame.display.set_mode(size) #create a Surface called "screen" with pygame (the screen the computer displays)
+
 class Player:
     """The class for the Player."""
     def __init__(self, name, appearance, eye_color, hair_color, weapon, color, equipped=None):
