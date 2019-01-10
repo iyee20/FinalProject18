fa14107ec4e08510c55fe92193c558122afa4acf
Wai Yee <iyee@ucvts.org>
2019-01-09 20:18:55 -0500

    Test Marth image again. It works. Cry tears of joy.

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 #A file for testing code.
 import sys, pygame
-screen = pygame.display.set_mode((3000, 3000))
+screen = pygame.display.set_mode((500, 500))
 
 marth_img = pygame.image.load("FEH_Marth.png").convert_alpha() #load image as surface
+x = 0
 while True:
+    x += 1
     screen.fill((0,0,0))
     screen.blit(marth_img, (0,0))
+    pygame.display.flip()
